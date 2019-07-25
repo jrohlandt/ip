c984682080615bd4e04d4eca9c67ebfe9d5e783d
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-07-25 22:45:35 +0200

    logout

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,5 @@ Route::get('watch/{projectId}', 'PlayerController@watch');
 
 
 Auth::routes();
-
+Route::get('logout', 'Auth\LoginController@logout');
 Route::get('/home', 'HomeController@index')->name('home');
