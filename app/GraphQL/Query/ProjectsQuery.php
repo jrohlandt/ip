823ece3d300e60832b425bea24ef32b8e8485405
Jeandre Rohlandt <jrohlandt@gmail.com>
2019-08-18 13:31:09 +0200

    install rebing/graphql

--- app/GraphQL/Query/ProjectsQuery.php
+++ app/GraphQL/Query/ProjectsQuery.php
@@ -3,8 +3,8 @@
 namespace App\GraphQL\Query;
 
 use App\Project;
-use Folklore\GraphQL\Support\Query;
-use GraphQL;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
 
@@ -12,20 +12,21 @@ class ProjectsQuery extends Query
 {
     protected $attributes = [
         'name' => 'projects',
-        'description' => 'get all projects'
+        'description' => 'get all projects',
+        'model' => Project::class,
     ];
 
-    public function type()
+    public function type(): Type
     {
         return Type::listOf(GraphQL::type('Project'));
     }
 
-    public function args()
-    {
-        return [
-            
-        ];
-    }
+//    public function args(): array
+//    {
+//        return [
+//
+//        ];
+//    }
 
     public function resolve($root, $args, $context, ResolveInfo $info)
     {
--- app/GraphQL/Type/ProjectType.php
+++ app/GraphQL/Type/ProjectType.php
@@ -2,22 +2,28 @@
 
 namespace App\GraphQL\Type;
 
+use App\Project;
 use GraphQL\Type\Definition\Type;
-use Folklore\GraphQL\Support\Type as GraphQLType;
+use Rebing\GraphQL\Support\Type as GraphQLType;
 
 class ProjectType extends GraphQLType
 {
     protected $attributes = [
-      'name' => 'Project',
-      'description' => 'A project'
+        'name' => 'Project',
+        'description' => 'A project',
+        'model' => Project::class
     ];
 
-    public function fields()
+    public function fields(): array
     {
         return [
             'id' => [
                 'type' => Type::nonNull(Type::int()),
                 'description' => 'MySQL id of the projects row'
+            ],
+            'user_id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The project belongs to this user'
             ],
             'title' => [
                 'type' => Type::nonNull(Type::string()),
@@ -29,4 +35,6 @@ class ProjectType extends GraphQLType
             ],
         ];
     }
+
+
 }
